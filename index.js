5fdd2da7d20915c19acbd7ec0f8546c9915a9692
Steven Liu <lxlsteven011212@gmail.com>
2021-08-25 13:56:51 -0400

    typo fixed

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.use(cors());
 app.use('/posts', postRoutes);
 
 app.get('/', (req, res) => {
-  res.send('Hello to qa API');
+  res.send('Hello to my API');
 });
 
 const PORT = process.env.PORT;
